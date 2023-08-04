51274fd001365319acba9c3f267e859e8570c5cd
Erick Cohen <erickdcohen@gmail.com>
2023-08-04 16:17:50 -0400

    Adds more functionality on adding and replacing elements on main page

---
+++ src/components/Recipe.js
@@ -0,0 +1,6 @@
+export default class Recipe {
+	constructor(name, url) {
+		this.name = name
+		this.url = url
+	}
+}
--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import AddRecipe from './components/AddRecipe.js'
+import Recipe from './components/Recipe.js'
 
 // Get top nav buttons
 const searchRecipiesButtons = document.querySelectorAll('.header-list-item')
@@ -18,19 +18,70 @@ searchRecipiesButtons.forEach((button) => {
 // get main display area
 const mainDisplay = document.getElementById('main-display-container')
 
+// build search and search functionality
+const buildSearchForm = () => {
+	const form = document.createElement('form')
+	const recipeURLInput = document.createElement('input')
+	recipeURLInput.classList.add('search-form-text-input')
+
+	form.appendChild(recipeURLInput)
+	mainDisplay.appendChild(form)
+}
+
+const addNewRecipe = () => {
+	const newRecipieForm = document.createElement('form')
+	const recipeNameInput = document.createElement('input')
+
+	recipeNameInput.classList.add('new-recipe-form-input')
+	recipeNameInput
+
+	newRecipieForm.appendChild(recipeNameInput)
+
+	mainDisplay.appendChild(newRecipieForm)
+}
+
+const generateRandomRecipe = () => {}
+
+// Display each option
 const displaySearch = () => {
-	mainDisplay.textContent = 'Search'
+	if (mainDisplay.children.length > 0) {
+		mainDisplay.innerHTML = ''
+	}
+
+	const heading = document.createElement('h1')
+	heading.classList.add('display-section-heading')
+	heading.innerText = 'Search'
+	mainDisplay.appendChild(heading)
+
+	buildSearchForm()
+	// TODO add search functionality
 }
 
 const displayGenerateRandom = () => {
-	mainDisplay.textContent = 'Generate Random'
+	if (mainDisplay.children.length > 0) {
+		mainDisplay.innerHTML = ''
+	}
+
+	const heading = document.createElement('h1')
+	heading.classList.add('display-section-heading')
+	heading.innerText = 'Get Random recipe'
+	mainDisplay.appendChild(heading)
+
+	generateRandomRecipe()
 }
 
 const displayAdd = () => {
-	mainDisplay.textContent = 'Add Recipe'
+	if (mainDisplay.children.length > 0) {
+		mainDisplay.innerHTML = ''
+	}
+
+	const heading = document.createElement('h1')
+	heading.classList.add('display-section-heading')
+	heading.innerText = 'Add new recipe'
+
+	mainDisplay.appendChild(heading)
+	addNewRecipe()
 }
 
 // Always start with Search
-displaySearch()
-
-const addRecipe = new AddRecipe()
+// displaySearch()
